c06fb0b4b2b81756d22b34cae790f6e849a79f90
Alexandre Rojon <alexandre.rojon@gmail.com>
2022-01-22 18:42:15 +0100

    Views & controller methods

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,9 +1,17 @@
 class SearchesController < ApplicationController
+    
     def new
+        @my_variable = ENV["POLYGRAPH_API_KEY"]
+        response = RestClient.get 'http://https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2020-06-01/2020-06-17?apiKey=ENV["POLYGRAPH_API_KEY"].com/resource'
+        user_searched_ticker = JSON.parse response
+        
+        
+
 
     end
 
     def show
         search_term = params['q'].capitalize
     end
+
 end
